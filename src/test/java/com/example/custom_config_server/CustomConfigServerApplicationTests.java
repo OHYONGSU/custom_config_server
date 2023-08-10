0af4a5d124cf5ebcb174f93023ee7136b0c1328a
OHYONGSU <yongsu@jobis.co>
2023-08-10 11:28:24 +0900

    Initial commit

---
+++ src/test/java/com/example/custom_config_server/CustomConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.custom_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomConfigServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
